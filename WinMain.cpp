7a16e5adb2041cb9f889174f932f9e901f3eff33
Diogo Duarte <diogo8500@gmail.com>
2019-10-31 00:42:11 +0000

    Update

--- WinMain.cpp
+++ WinMain.cpp
@@ -16,7 +16,7 @@ int CALLBACK WinMain(
 	}
 	catch (const std::exception & e)
 	{
-		MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION); 
+		MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
 	}
 	catch (...)
 	{
